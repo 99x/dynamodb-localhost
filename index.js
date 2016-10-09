e87f3c9f66b14abcf88bb5b1c2c88fd56d894b84
Manoj Fernando <manojf@99x.lk>
2016-10-09 22:50:55 +0530

    Adding optional db install path.

--- index.js
+++ index.js
@@ -7,39 +7,42 @@ var installer = require('./dynamodb/installer'),
     dbInstances = {};
 
 var dynamodb = {
-    install: function (callback) {
-        installer.install(config, function (msg) {
+    install: function(callback, path) {
+        if (path) {
+            config.setup.install_path = path;
+        }
+        installer.install(config, function(msg) {
             console.log(msg);
             callback();
         });
     },
-    start: function (options) {
+    start: function(options) {
         var instance = starter.start(options, config);
         dbInstances[instance.port] = {
             process: instance.proc,
             options: options
         };
-        instance.proc.on('close', function (code) {
+        instance.proc.on('close', function(code) {
             if (code !== null && code !== 0) {
                 console.log('DynamoDB Local failed to start with code', code);
             }
         });
         console.log('Dynamodb Local Started, Visit: http://localhost:' + (options.port || config.start.port) + '/shell');
     },
-    stop: function (port) {
+    stop: function(port) {
         if (dbInstances[port]) {
             dbInstances[port].process.kill('SIGKILL');
             delete dbInstances[port];
         }
     },
-    restart: function (port) {
+    restart: function(port) {
         var options = dbInstances[port].options;
         this.stop(port);
         this.start(options);
         console.log("Successfully restarted dynamodb local on port: " + port);
     },
-    remove: function (callback) {
-        utils.removeDir(config.setup.install_path, function () {
+    remove: function(callback) {
+        utils.removeDir(config.setup.install_path, function() {
             console.log("Successfully removed dynamodb local!");
             callback();
         });

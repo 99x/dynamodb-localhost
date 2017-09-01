cd1d3e3ccc68414f6927ee5d2dd4e728688fd988
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-09-01 22:54:40 +0530

    Fix indentation to 2 spaces Fix #14

--- dynamodb/utils.js
+++ dynamodb/utils.js
@@ -1,26 +1,26 @@
 'use strict';
 
 var path = require('path'),
-    rmdir = require('rmdir'),
-    fs = require('fs');
+  rmdir = require('rmdir'),
+  fs = require('fs');
 
 var absPath = function (relPath) {
-    return path.dirname(__filename) + '/' + relPath;
+  return path.dirname(__filename) + '/' + relPath;
 };
 
 var removeDir = function (relPath, callback) {
-    var path = absPath(relPath);
-    rmdir(path, callback);
+  var path = absPath(relPath);
+  rmdir(path, callback);
 };
 
 var createDir = function (relPath) {
-    if (!fs.existsSync(absPath(relPath))) {
-        fs.mkdirSync(absPath(relPath));
-    }
+  if (!fs.existsSync(absPath(relPath))) {
+    fs.mkdirSync(absPath(relPath));
+  }
 };
 
 module.exports = {
-    absPath: absPath,
-    removeDir: removeDir,
-    createDir: createDir
+  absPath: absPath,
+  removeDir: removeDir,
+  createDir: createDir
 };

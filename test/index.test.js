9b4d2ba56cd9f9a68cbca6ee6c4096e7ab39101c
Kolya Venturi <kolya.venturi@enx3s.com>
2023-04-17 17:57:23 -0700

    Add async install method

--- test/index.test.js
+++ test/index.test.js
@@ -1,21 +1,37 @@
 const fs = require("fs");
 const utils = require("../dynamodb/utils");
 const { expect } = require("chai");
-const { install, remove } = require("../index");
+const { install, remove, installAsync } = require("../index");
 const config = require("../dynamodb/config.json");
 
 describe("install", function() {
   const installPath = `./dynamodb/${config.setup.install_path}`;
   const jarPath = `${installPath}/${config.setup.jar}`;
 
+  beforeEach(() => {
+    if (fs.existsSync(jarPath)) {
+      fs.unlinkSync(jarPath);
+    }
+  });
+
   it("downloads the jar successfully", function(done) {
-    this.timeout(10000); // 10 second timeout
+    this.timeout(60000); // 60 second timeout
 
     install(function() {
       if (fs.existsSync(jarPath)) {
         done();
       }
     });
+  });
+
+  it("can download the jar async", async function() {
+    this.timeout(60000); // 60s timeout
+
+    await installAsync();
+
+    if (!fs.existsSync(jarPath)) {
+      throw new Error('Jar was not downloaded.');
+    }
   });
 
   it("removes the setup files", function(done) {
